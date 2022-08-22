f08a533a60b300bceb6b64560ea5329dfe15eeb3
DESKTOP-TV3PTH1\Renegaat <venecaat@gmail.com>
2022-08-22 11:26:56 +0200

    merge

---
+++ data_manager.py
@@ -0,0 +1,343 @@
+import os
+
+import connection
+from datetime import datetime
+
+
+# QUESTION_COLUMNS = ['id', 'submission_time', "view_number", "vote_number", "title", "message", "image"]
+# ANSWER_COLUMNS = ["id", "submission_time", "vote_number", "question_id", "message", "image"]
+# COMMENT_COLUMNS = ["id", "question_id", "answer_id", "message", "submission_time", "edited_count"]
+# TAG_COLUMNS = ["id", "name"]
+
+
+def sort_questions(views=False, votes=False, submission=False, desc=False):
+    questions = get_questions_db()
+
+    if views:
+        return sorted(questions, key=lambda x: int(x['view_number']), reverse=desc)
+    elif votes:
+        return sorted(questions, key=lambda x: int(x['vote_number']), reverse=desc)
+    elif submission or not submission:
+        return sorted(questions, key=lambda x: x['submission_time'], reverse=desc)
+
+
+def get_time():
+    curr_date = datetime.now()
+    norm_time = curr_date.strftime("%Y/%m/%d %H:%M:%S")
+
+    return norm_time
+
+
+# ----------------------------------------------------------------------------------------------------
+# Get all question/answer/comment/tag from corresponding tables
+
+@connection.connection_handler
+def get_questions_db(cursor):
+    cursor.execute("SELECT * FROM question")
+    return cursor.fetchall()
+
+
+@connection.connection_handler
+def get_questions_ids(cursor):
+    cursor.execute("SELECT id FROM question")
+    return cursor.fetchall()
+
+
+@connection.connection_handler
+def get_answers_db(cursor, question_id):
+    cursor.execute(f"SELECT * FROM answer WHERE question_id = {question_id} ORDER BY id")
+    return cursor.fetchall()
+
+
+@connection.connection_handler
+def get_comments_for_question_db(cursor, question_id):
+    cursor.execute(f"SELECT * FROM comment WHERE question_id = {question_id} ORDER BY id")
+    return cursor.fetchall()
+
+
+@connection.connection_handler
+def get_comments_for_answers_db(cursor, answers):
+    comments = []
+    for ans in answers:
+        cursor.execute(f"""
+        SELECT id, question_id, answer_id, submission_time, message, edited_count
+        FROM comment
+        WHERE answer_id = {ans['id']}
+        ORDER BY id
+        """)
+        comments.append(cursor.fetchall())
+    return comments
+
+
+# ----------------------------------------------------------------------------------------------------
+# Add question/answer/comment/tag to corresponding tables
+
+@connection.connection_handler
+def add_question_db(cursor, new_question):
+    cursor.execute(
+        f"""
+        INSERT INTO question
+        VALUES(%s, %s, %s, %s, %s, %s, %s)
+        """,
+        (new_question['id'], new_question['submission_time'], new_question['view_number'], new_question['vote_number'],
+         new_question['title'], new_question['message'], new_question['image'])
+    )
+
+
+@connection.connection_handler
+def add_answer_db(cursor, new_answer):
+    cursor.execute(
+        """
+        INSERT INTO answer
+        VALUES(%s, %s, %s, %s, %s, %s)
+        """,
+        (new_answer['id'], new_answer['submission_time'], new_answer['vote_number'], new_answer['question_id'],
+         new_answer['message'], new_answer['image'])
+    )
+
+
+@connection.connection_handler
+def add_comment_db(cursor, new_comment):
+    if "question_id" in new_comment.keys():
+        cursor.execute("""
+            INSERT INTO comment (id, question_id, message, submission_time, edited_count)
+            VALUES(%s, %s, %s, %s, %s)""", (new_comment['id'], new_comment['question_id'], new_comment['message'],
+                                            new_comment['submission_time'], new_comment['edited_count'])
+                       )
+    else:
+        cursor.execute("""
+            INSERT INTO comment (id, answer_id, message, submission_time, edited_count)
+            VALUES(%s, %s, %s, %s, %s)""", (new_comment['id'], new_comment['answer_id'], new_comment['message'],
+                                            new_comment['submission_time'], new_comment['edited_count'])
+                       )
+
+
+# ----------------------------------------------------------------------------------------------------
+# Get next ID for question/answer/comment/tag from corresponding tables
+
+@connection.connection_handler
+def get_next_question_id(cursor):
+    cursor.execute("SELECT MAX(id) as id FROM question")
+    return cursor.fetchall()[0]['id'] + 1
+
+
+@connection.connection_handler
+def get_next_answer_id(cursor):
+    cursor.execute("SELECT MAX(id) as id FROM answer")
+    return cursor.fetchall()[0]['id'] + 1
+
+
+@connection.connection_handler
+def get_next_comment_id(cursor):
+    cursor.execute("SELECT MAX(id) as id FROM comment")
+    return cursor.fetchall()[0]['id'] + 1
+
+
+# ----------------------------------------------------------------------------------------------------
+# Delete question/answer/comment from corresponding tables
+
+@connection.connection_handler
+def delete_question(cursor, question_id):
+    cursor.execute(f"SELECT image FROM answer WHERE question_id = {question_id}")
+    answer_images = cursor.fetchall()
+    cursor.execute(f"""
+    DELETE FROM comment WHERE answer_id IN
+    (SELECT answer.id FROM answer JOIN question ON answer.question_id = question.id
+     WHERE answer.question_id = {question_id})
+    """)
+    cursor.execute(f"DELETE FROM comment WHERE question_id = {question_id}")
+    cursor.execute(f"DELETE FROM question_tag WHERE question_id = {question_id}")
+    cursor.execute(f"DELETE FROM answer WHERE question_id = {question_id}")
+    cursor.execute(f"DELETE FROM question WHERE id = {question_id}")
+
+    for d in answer_images:
+        os.remove(['image'])
+
+
+@connection.connection_handler
+def delete_answer_and_comments(cursor, answer_id):
+    cursor.execute(f"DELETE FROM comment WHERE answer_id = {answer_id}")
+    cursor.execute(f"DELETE FROM answer WHERE id = {answer_id}")
+
+
+@connection.connection_handler
+def delete_comment(cursor, comment_id):
+    cursor.execute(f"DELETE FROM comment WHERE id = {comment_id}")
+
+
+# ----------------------------------------------------------------------------------------------------
+# Get question/answer/comment/tag by ID from corresponding tables
+
+@connection.connection_handler
+def get_question_by_id(cursor, question_id):
+    cursor.execute(f"SELECT * FROM question WHERE id = {question_id}")
+    return cursor.fetchall()[0]
+
+
+@connection.connection_handler
+def get_answer_by_id(cursor, answer_id):
+    cursor.execute(f"SELECT * FROM answer WHERE id = {answer_id}")
+    return cursor.fetchall()[0]
+
+
+@connection.connection_handler
+def get_comment_by_id(cursor, comment_id):
+    cursor.execute(f"SELECT * FROM comment WHERE id = {comment_id}")
+    return cursor.fetchall()[0]
+
+
+# ----------------------------------------------------------------------------------------------------
+# Get an answer's/comment's question_id
+
+@connection.connection_handler
+def get_answer_question_id(cursor, answer_id):
+    cursor.execute(f"SELECT question_id FROM answer WHERE id = {answer_id}")
+    return cursor.fetchall()[0]["question_id"]
+
+
+@connection.connection_handler
+def get_comment_question_id(cursor, comment_id):
+    cursor.execute(f"""
+    SELECT
+    CASE
+        WHEN comment.question_id IS NOT NULL THEN comment.question_id
+        ELSE answer.question_id
+    END AS question_id
+    FROM comment
+    LEFT JOIN answer ON comment.answer_id = answer.id
+    WHERE comment.id = {comment_id}
+    LIMIT 1
+    """)
+    return cursor.fetchall()[0]["question_id"]
+
+
+# ----------------------------------------------------------------------------------------------------
+# Modify question/answer/comment/tag in corresponding tables
+
+@connection.connection_handler
+def modify_value_in_question(cursor, question_id, voting="", view="", edited_question=False, title="", message=""):
+    if view:
+        cursor.execute(f"UPDATE question SET view_number = view_number + 1 WHERE id = {question_id}")
+    elif voting == "vote-up":
+        cursor.execute(f"UPDATE question SET vote_number = vote_number + 1 WHERE id = {question_id}")
+    elif voting == "vote-down":
+        cursor.execute(f"UPDATE question SET vote_number = vote_number - 1 WHERE id = {question_id}")
+    if edited_question:
+        cursor.execute(f"UPDATE question SET title = '{title}', message = '{message}' WHERE id = {question_id}")
+
+
+@connection.connection_handler
+def modify_value_in_answer(cursor, answer_id, voting="", edited_answer=False, message=""):
+    if voting == "vote-up":
+        cursor.execute(f"UPDATE answer SET vote_number = vote_number + 1 WHERE id = {answer_id}")
+    elif voting == "vote-down":
+        cursor.execute(f"UPDATE answer SET vote_number = vote_number - 1 WHERE id = {answer_id}")
+    if edited_answer:
+        cursor.execute(f"UPDATE answer SET message = '{message}' WHERE id = {answer_id}")
+
+
+@connection.connection_handler
+def modify_value_in_comment(cursor, comment_id, message, submission_time):
+    cursor.execute(f"""
+    UPDATE comment SET message = '{message}', edited_count = edited_count + 1,
+    submission_time = '{submission_time}'
+    WHERE id = {comment_id}
+    """)
+
+
+# ----------------------------------------------------------------------------------------------------
+# Search results
+
+@connection.connection_handler
+def get_latest_questions(cursor):
+    cursor.execute(
+        """
+        SELECT * FROM question
+        ORDER BY submission_time DESC
+        LIMIT 5
+        """
+    )
+
+    return cursor.fetchall()
+
+
+@connection.connection_handler
+def get_search_results(cursor, search_phrase):
+    cursor.execute(
+        """
+        SELECT DISTINCT question.* FROM question
+        LEFT JOIN answer ON question.id = answer.question_id
+        WHERE question.title ILIKE %(srch_ph)s OR question.message ILIKE %(srch_ph)s OR answer.message ILIKE %(srch_ph)s
+        """,
+        {'srch_ph': '%{}%'.format(search_phrase)}
+    )
+
+    return cursor.fetchall()
+
+
+# ----------------------------------------------------------------------------------------------------
+# Tags
+
+
+@connection.connection_handler
+def get_all_tags(cursor):
+    cursor.execute("SELECT name FROM tag")
+
+    return cursor.fetchall()
+
+
+@connection.connection_handler
+def add_tag(cursor, tag_name):
+    cursor.execute(f"INSERT INTO tag(name) VALUES('{tag_name}') WHERE '{tag_name}' NOT IN tag(name)")
+
+
+@connection.connection_handler
+def delete_tag_from_question(cursor, question_id, tag_id):
+    cursor.execute(
+        f"""
+        DELETE FROM question_tag
+        WHERE question_id = {question_id} AND tag_id = {tag_id}
+        """
+    )
+
+
+@connection.connection_handler
+def get_tag_id(cursor, tag_name):
+    cursor.execute(f"SELECT id FROM tag WHERE name = '{tag_name}'")
+
+    return cursor.fetchall()
+
+
+@connection.connection_handler
+def add_tag_to_question(cursor, question_id, tag_id):
+    cursor.execute(
+        f"""
+        INSERT INTO question_tag(question_id, tag_id)
+        VALUES('{question_id}', '{tag_id}')
+        ON CONFLICT (tag_id, question_id) DO NOTHING
+        """
+    )
+
+
+@connection.connection_handler
+def add_custom_tag(cursor, tag_name):
+    cursor.execute(f"SELECT name FROM tag WHERE name LIKE '{tag_name}'")
+    found_tag = cursor.fetchall()
+
+    if not found_tag:
+        cursor.execute(
+            f"""
+            INSERT INTO tag(name)
+            VALUES('{tag_name}')
+            """)
+
+
+@connection.connection_handler
+def get_tags_for_question(cursor, question_id):
+    cursor.execute(
+        f"""
+        SELECT tag.* FROM tag
+        JOIN question_tag ON tag.id = question_tag.tag_id
+        WHERE question_tag.question_id = {question_id};
+        """)
+    return cursor.fetchall()
--- server.py
+++ server.py
@@ -1,13 +1,315 @@
-from flask import Flask, render_template
-from bonus_questions import SAMPLE_QUESTIONS
+from flask import Flask, render_template, redirect, request
+import data_manager, os
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = "/static/resources/uploaded-images"
+ALLOWED_EXT = {'png', 'jpg', 'jpeg'}
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
-@app.route("/bonus-questions")
-def main():
-    return render_template('bonus_questions.html', questions=SAMPLE_QUESTIONS)
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXT
+
+
+@app.route("/", methods=['GET', 'POST'])
+def main_page():
+    latest_qs = data_manager.get_latest_questions()
+
+    return render_template("index.html", questions=latest_qs)
+
+
+@app.route("/list", methods=['GET', 'POST'])
+def list_questions():
+    if "sort_by" in request.url:
+        if request.args['sort_by'] == "views-desc":
+            questions = data_manager.sort_questions(views=True, desc=True)
+        elif request.args['sort_by'] == "views-asc":
+            questions = data_manager.sort_questions(views=True)
+        elif request.args['sort_by'] == "votes-desc":
+            questions = data_manager.sort_questions(votes=True, desc=True)
+        elif request.args['sort_by'] == "votes-asc":
+            questions = data_manager.sort_questions(votes=True)
+        elif request.args['sort_by'] == "submission-desc":
+            questions = data_manager.sort_questions(submission=True, desc=True)
+        elif request.args['sort_by'] == "submission-asc":
+            questions = data_manager.sort_questions(submission=True)
+    else:
+        questions = data_manager.sort_questions(submission=True, desc=True)
+
+    # questions_tags = []
+    # for que in questions:
+    #     questions_tags.append(data_manager.get_tags_for_question(question_id=que['id']))
+
+    return render_template("list.html", questions=questions)
+
+
+@app.route("/add-question", methods=['GET', 'POST'])
+def add_question():
+    question = {}
+
+    if request.method == "POST":
+        question["id"] = data_manager.get_next_question_id()
+        question["submission_time"] = data_manager.get_time()
+        question["view_number"] = 0
+        question["vote_number"] = 0
+        question["title"] = request.form["question"]
+        question["message"] = request.form["description"]
+
+        if request.files['img']:
+            img = request.files['img']
+            if img and allowed_file(img.filename):
+                filename = secure_filename(img.filename)
+
+                absolute_path = os.path.dirname(__file__)
+                relative_path = "static\\resources\\uploaded-images"
+                full_path = os.path.join(absolute_path, relative_path)
+
+                img.save(os.path.join(full_path, filename))
+
+            question["image"] = f"static/resources/uploaded-images/{filename}"
+        else:
+            question['image'] = ""
+
+        question_id = question["id"]
+        data_manager.add_question_db(new_question=question)
+
+        return redirect(f"/question/{question_id}")
+
+    return render_template("add-question.html")
+
+
+@app.route("/question/<int:question_id>", methods=["POST", "GET"])
+def display_question(question_id):
+    if request.method == "POST":
+        if request.form["to_question"] == "to-question":
+            data_manager.modify_value_in_question(question_id=question_id, view="to-question")
+        if 'tag_name' in request.form:
+            tag_to_add = request.form['tag_name']
+            tag_id = data_manager.get_tag_id(tag_to_add)
+            data_manager.add_tag_to_question(question_id, tag_id)
+
+    tags_for_question = data_manager.get_tags_for_question(question_id)
+    all_tags = data_manager.get_all_tags()
+    question_of_given_id = data_manager.get_question_by_id(question_id)
+    answers = data_manager.get_answers_db(question_id)
+    comments_for_question = data_manager.get_comments_for_question_db(question_id)
+    comments_for_answers = data_manager.get_comments_for_answers_db(answers)
+
+    """ SYNTAX FOR COMMENTS_FOR_ANSWERS - NEED 3 FOR LOOP
+    all comment list []
+       comments for an answer list []
+          comment elements dictionary {}
+    """
+
+    return render_template('question.html', question_of_given_id=question_of_given_id, answers_to_question=answers,
+                           comments_for_question=comments_for_question, comments_for_answers=comments_for_answers,
+                           question_id=question_id, tags_for_question=tags_for_question, all_tags=all_tags)
+
+
+@app.route("/question_voting/<int:question_id>", methods=['POST'])
+def question_voting(question_id):
+    if request.method == "POST":
+        if request.form["vote"] == "vote-up":
+            data_manager.modify_value_in_question(question_id=question_id, voting="vote-up")
+
+        elif request.form['vote'] == "vote-down":
+            data_manager.modify_value_in_question(question_id=question_id, voting="vote-down")
+
+        if request.form["page"] == "list":
+            return redirect("/list")
+
+        elif request.form["page"] == "question":
+            return redirect(f"/question/{question_id}")
+
+
+@app.route("/delete_question/<int:question_id>")
+def delete_question(question_id):
+    sel_que = data_manager.get_question_by_id(question_id)
+    img_path = sel_que['image']
+    if img_path:
+        os.remove(img_path)
+    data_manager.delete_question(question_id)
+
+    return redirect("/list")
+
+
+@app.route("/question/<int:question_id>/edit", methods=['GET', 'POST'])
+def edit_question(question_id):
+    if request.method == "POST":
+        modified_title = request.form["question"]
+        modified_message = request.form["description"]
+
+        data_manager.modify_value_in_question(question_id=question_id, edited_question=True, title=modified_title,
+                                              message=modified_message)
+
+        return redirect(f"/question/{question_id}")
+    else:
+        question_of_given_id = data_manager.get_question_by_id(question_id)
+
+    return render_template("edit-question.html", question_for_edit=question_of_given_id)
+
+
+@app.route("/question/<int:question_id>/new-answer", methods=["POST", "GET"])
+def add_answer(question_id):
+    answer = {}
+
+    if request.method == "POST":
+        answer["id"] = data_manager.get_next_answer_id()
+        answer["submission_time"] = data_manager.get_time()
+        answer["vote_number"] = 0
+        answer["question_id"] = question_id
+        answer["message"] = request.form["description"]
+
+        if request.files['img']:
+            img = request.files['img']
+            if img and allowed_file(img.filename):
+                filename = secure_filename(img.filename)
+
+                absolute_path = os.path.dirname(__file__)
+                relative_path = "static\\resources\\uploaded-images"
+                full_path = os.path.join(absolute_path, relative_path)
+
+                img.save(os.path.join(full_path, filename))
+
+            answer["image"] = f"static/resources/uploaded-images/{filename}"
+        else:
+            answer['image'] = ""
+
+        data_manager.add_answer_db(answer)
+
+        return redirect(f"/question/{question_id}")
+
+    return render_template("add-answer.html", question_id=question_id)
+
+
+@app.route("/answer/<int:answer_id>/delete")
+def delete_answer(answer_id):
+    question_id = data_manager.get_answer_question_id(answer_id)
+    ans_que = data_manager.get_answer_by_id(answer_id)
+    img_path = ans_que['image']
+    if img_path:
+        os.remove(img_path)
+    data_manager.delete_answer_and_comments(answer_id)
+
+    return redirect(f"/question/{question_id}")
+
+
+@app.route("/answer/<int:answer_id>/edit", methods=['GET', 'POST'])
+def edit_answer(answer_id):
+    if request.method == "POST":
+        modified_message = request.form["message"]
+        ans_question_id = data_manager.get_answer_question_id(answer_id)
+
+        data_manager.modify_value_in_answer(answer_id=answer_id, edited_answer=True, message=modified_message)
+
+        return redirect(f"/question/{ans_question_id}")
+    else:
+        answer_of_given_id = data_manager.get_answer_by_id(answer_id)
+
+    return render_template("edit-answer.html", answer_for_edit=answer_of_given_id)
+
+
+@app.route("/answer_voting/<int:answer_id>", methods=['POST'])
+def answer_voting(answer_id):
+    ans_question_id = data_manager.get_answer_question_id(answer_id)
+    if request.form["vote"] == "vote-up":
+        data_manager.modify_value_in_answer(answer_id=answer_id, voting="vote-up")
+
+    elif request.form['vote'] == "vote-down":
+        data_manager.modify_value_in_answer(answer_id=answer_id, voting="vote-down")
+
+    return redirect(f"/question/{ans_question_id}")
+
+
+@app.route("/search", methods=['POST'])
+def search():
+    search_phrase = request.form["search_phrase"]
+    matching_questions = data_manager.get_search_results(search_phrase)
+
+    return render_template("list.html", questions=matching_questions, search_phrase=search_phrase)
+
+
+@app.route("/answer/<int:answer_id>/new-comment", methods=["POST", "GET"])
+@app.route("/question/<int:question_id>/new-comment", methods=["POST", "GET"])
+def add_comment(question_id=-1, answer_id=-1):
+    comment = {}
+    if request.method == "POST":
+        comment["id"] = data_manager.get_next_comment_id()
+        if question_id >= 0:
+            comment["question_id"] = question_id
+        else:
+            question_id = data_manager.get_answer_question_id(answer_id)
+            comment["answer_id"] = answer_id
+
+        comment["message"] = request.form["description"]
+        comment["submission_time"] = data_manager.get_time()
+        comment["edited_count"] = 0
+
+        data_manager.add_comment_db(new_comment=comment)
+
+        return redirect(f"/question/{question_id}")
+
+    if question_id >= 0:
+        return render_template("add-comment.html", question_id=str(question_id))
+    else:
+        return render_template("add-comment.html", answer_id=str(answer_id))
+
+
+@app.route("/comment/<int:comment_id>/delete")
+def delete_comment(comment_id):
+    question_id = data_manager.get_comment_question_id(comment_id)
+    data_manager.delete_comment(comment_id)
+
+    return redirect(f"/question/{question_id}")
+
+
+@app.route("/comment/<int:comment_id>/edit", methods=['GET', 'POST'])
+def edit_comment(comment_id):
+    if request.method == "POST":
+        modified_message = request.form["description"]
+        modified_submission_time = data_manager.get_time()
+        comment_question_id = data_manager.get_comment_question_id(comment_id)
+
+        data_manager.modify_value_in_comment(comment_id=comment_id, message=modified_message,
+                                             submission_time=modified_submission_time)
+
+        return redirect(f"/question/{comment_question_id}")
+    else:
+        comment_of_given_id = data_manager.get_comment_by_id(comment_id)
+
+    return render_template("edit-comment.html", comment_for_edit=comment_of_given_id)
+
+
+@app.route('/question/<int:question_id>/new-tag', methods=["POST", "GET"])
+def add_tag(question_id):
+    all_tags = data_manager.get_all_tags()
+    if request.method == "POST":
+        if 'custom_tag_name' in request.form:
+            tag_to_add = request.form['custom_tag_name']
+            data_manager.add_custom_tag(tag_to_add)
+            tag_id = data_manager.get_tag_id(tag_to_add)[0]['id']
+            data_manager.add_tag_to_question(question_id, tag_id)
+        elif 'tag_name' in request.form:
+            tag_to_add = request.form['tag_name']
+            tag_id = data_manager.get_tag_id(tag_to_add)[0]['id']
+            data_manager.add_tag_to_question(question_id, tag_id)
+        return redirect(f"/question/{question_id}")
+
+    return render_template("add-tag.html", tags=all_tags, question_id=question_id)
+
+
+@app.route('/question/<int:question_id>/tag/<int:tag_id>/delete', methods=['POST'])
+def delete_tag(question_id, tag_id):
+    data_manager.delete_tag_from_question(question_id, tag_id)
+
+    return redirect(f'/question/{question_id}')
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(
+        host='0.0.0.0',
+        port=8000,
+        debug=True,
+    )
